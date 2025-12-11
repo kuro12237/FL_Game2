10a2d749a1766db023ae7740dab793f7252090b0
Tsumita-Hinata0219 <k022g0114@g.neec.ac.jp>
2025-12-11 18:22:25 +0900

    numberのtexture追加

--- project/Application/GameObject/Select/StageSelectUI.cpp
+++ project/Application/GameObject/Select/StageSelectUI.cpp
@@ -9,10 +9,30 @@ StageSelectUI::StageSelectUI()
 
 void StageSelectUI::Init()
 {
-   uint32_t noiseTexHandle = TextureManager::LoadPngTexture("GameObject/Noise/Noise.png");
+   uint32_t selectnum1 = TextureManager::LoadPngTexture("SelectNumber/selectNum_1.png");
+   uint32_t selectnum2 = TextureManager::LoadPngTexture("SelectNumber/selectNum_2.png");
+   uint32_t selectnum3 = TextureManager::LoadPngTexture("SelectNumber/selectNum_3.png");
+   uint32_t selectnum4 = TextureManager::LoadPngTexture("SelectNumber/selectNum_4.png");
+   uint32_t selectnum5 = TextureManager::LoadPngTexture("SelectNumber/selectNum_5.png");
+   uint32_t selectnum6 = TextureManager::LoadPngTexture("SelectNumber/selectNum_6.png");
+   uint32_t selectnum7 = TextureManager::LoadPngTexture("SelectNumber/selectNum_7.png");
+   uint32_t selectnum8 = TextureManager::LoadPngTexture("SelectNumber/selectNum_8.png");
+   uint32_t selectnum9 = TextureManager::LoadPngTexture("SelectNumber/selectNum_9.png");
+   uint32_t selectnum10 = TextureManager::LoadPngTexture("SelectNumber/selectNum_10.png");
+
+   selectnum1;
+   selectnum2;
+   selectnum3;
+   selectnum4;
+   selectnum5;
+   selectnum6;
+   selectnum7;
+   selectnum8;
+   selectnum9;
+   selectnum10;
 
    sprite_->Initialize();
-   sprite_->SetTexHandle(noiseTexHandle);
+   sprite_->SetTexHandle(selectnum1);
 
    worldTransform_.Initialize();
 }
